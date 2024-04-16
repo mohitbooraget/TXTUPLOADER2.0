9018803cacdb474024e39436d5dd4d7119f75114
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-04-16 09:56:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25322308"
-api_hash = "4f720e70ce155df40213026c31ccd819"
-bot_token = "7160376896:AAETYL8SJt-tDjTyxGStYFQHpXrdt6IiJ8g"
+api_id = "24147139"
+api_hash = "17f525f59de52108805a65f5ffd909e4"
+bot_token = "6939155705:AAGrrUjiqHkj9PP9XaFBS0exzHKOrWrdlwE"
